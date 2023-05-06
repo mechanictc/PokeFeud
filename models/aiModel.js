307970c043452907eb88522791dcc86eb695dd02
Daniel Weiss <gronkyd@gmail.com>
2023-05-06 12:33:10 -0700

    checkmove started

--- models/aiModel.js
+++ models/aiModel.js
@@ -1,25 +1,27 @@
-const pokemon = require('../pokemon.json');
+const pokemon = require('../pokemon.json')
 const moves = require('../moves');
-
 
 function teamCreate(){
 	const names = Object.keys(pokemon)
 	let finalTeam = []
 	for(let i=0; i<6; i++){
 		let randomSelect = Math.floor(Math.random() * 75);
-		const add = finalTeam.push(names[randomSelect])	
+		const add = finalTeam.push(names[randomSelect])
 	}
 	return finalTeam
 }
 
+function teamSwitch(team){
+	const kia = team.shift()
+	return team[0]
+}
 function checkMove(aiPokemon, playerPokemon){
 	// ARGUMENTS SHOULD BE CURRENTLY SELECETD POKEMON
-	const aiMoves = moves[aiPokemon];
-	const playerMoves = moves[playerPokemon];
+	const aiMoves = moves[aiPokemon].moveset;
+	const playerMoves = moves[playerPokemon].moveset;
 
-	for(move in aiMoves){
-		console.log(move);
+	for(move in playerMoves){
+		
 	}
 }
-
-module.exports = {teamCreate, checkMove};
+module.exports = {teamCreate, teamSwitch, checkMove};
