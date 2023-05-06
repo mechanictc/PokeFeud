6dc9e906aa2d15b870b33872f3f1f4a17fc4d654
MechanicTC2 <sciencenatekwok@gmail.com>
2023-05-05 20:46:47 -0700

    fix and revamp stuff

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const moves = require('../moves');
 const playModel = require('../models/playModel');
-const ai = require('../models/aiModel');
 const pokemon = require('../pokemon.json')
 
 /* GET home page. */
@@ -11,36 +10,7 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'PokeFever' });
 });
 
-// router.get('/play', function(req, res) {
-//   //pick teams
-//   var team1 = [];
-//   var team2 = [];
-//   team1 = ai.teamForm();
-//   team1 = team1.charAt(0).toUpperCase() + team1.slice(1);
-//   console.log(team1)
-//   team2 = ai.teamForm();
-//   var t1hp = ((team1.hp + 15) * 2 * 100 / 100) + 110;
-//   var t2hp = ((team2.hp + 15) * 2 * 100 / 100) + 110;
-//   var moveset;
-//   var info;
-//   var move1, move2, move3, move4;
-  
-//   try {
-//     moveset = moves[team1].moveset;
-//     info = Object.keys(moveset);
-//     move1 = info[0];
-//     move2 = info[1];
-//     move3 = info[2];
-//     move4 = info[3];
-//   }
-//   catch (err) {
-//   }
-//   console.log(moveset)
-
-//   if (team1.length != 0 && team2.length != 0) {
-//     res.render('game', { title: "PokeFront Battle V.S. CPU", team1: team1, team2: team2, move1: move1, move2: move2, move3: move3, move4: move4, moves: moves});
-//     }
-// });
-
-
+router.get('/play', function(req, res) {
+  res.render('game', {title: "test", calcDamage: playModel.calcDamage("charizard", "blastoise", "Flamethrower")});
+});
 module.exports = router;
