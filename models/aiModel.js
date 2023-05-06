218a337d4081eda50fe0a514cc4fb39738c9394c
Daniel Weiss <gronkyd@gmail.com>
2023-05-06 12:09:03 -0700

    l+DANIEL

--- models/aiModel.js
+++ models/aiModel.js
@@ -1,19 +1,25 @@
-const pokemon = require('../pokemon.json')
+const pokemon = require('../pokemon.json');
 const moves = require('../moves');
+
 
 function teamCreate(){
 	const names = Object.keys(pokemon)
 	let finalTeam = []
 	for(let i=0; i<6; i++){
 		let randomSelect = Math.floor(Math.random() * 75);
-		const add = finalTeam.push(names[randomSelect])
+		const add = finalTeam.push(names[randomSelect])	
 	}
 	return finalTeam
 }
 
-function teamSwitch(team){
-	const kia = team.shift()
-	return team[0]
+function checkMove(aiPokemon, playerPokemon){
+	// ARGUMENTS SHOULD BE CURRENTLY SELECETD POKEMON
+	const aiMoves = moves[aiPokemon];
+	const playerMoves = moves[playerPokemon];
+
+	for(move in aiMoves){
+		console.log(move);
+	}
 }
 
-module.exports = {teamCreate, teamSwitch};
+module.exports = {teamCreate, checkMove};
